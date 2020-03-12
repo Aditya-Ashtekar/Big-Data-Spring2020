365f2635e9260c4b6c417f6dba5dfa29033980b6
Aditya-Ashtekar <ama1219@nyu.edu>
2020-03-11 20:28:18 -0400

    Updated argument

--- assignment2/task4b.py
+++ assignment2/task4b.py
@@ -3,8 +3,7 @@ import pyspark
 from pyspark import SparkContext
 sc = SparkContext.getOrCreate("")
 from csv import reader
-Licenses = sc.textFile("task1b.out",1)
-#Licenses = sc.textFile(sys.argv[1],1)
+Licenses = sc.textFile(sys.argv[1],1)
 Licenses = Licenses.mapPartitions(lambda x:reader(x))
 
 Vehicle = Licenses.map(lambda x: (x[18],(x[3],x[5],x[8]))).distinct()
