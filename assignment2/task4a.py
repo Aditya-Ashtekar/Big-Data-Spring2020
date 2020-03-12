c7bf743610cf015161e0c65e9c1ea646242155fc
Aditya Ashtekar <adityaashtekar2012@gmail.com>
2020-03-11 21:10:22 -0400

    removed distinct

--- assignment2/task4a.py
+++ assignment2/task4a.py
@@ -6,7 +6,7 @@ from csv import reader
 
 Licenses = sc.textFile(sys.argv[1],1)
 Licenses = Licenses.mapPartitions(lambda x:reader(x))
-Vehicle = Licenses.map(lambda x: (x[16],(x[3],x[5],x[8]))).distinct()
+Vehicle = Licenses.map(lambda x: (x[16],(x[3],x[5],x[8])))
 
 r = Vehicle.map(lambda x: (x[0],(1,x[1][1],x[1][2])))
 res = r.reduceByKey(lambda x,y : (x[0]+y[0],float(x[1])+float(y[1]),float(x[2])+float(y[2]))).sortByKey()
