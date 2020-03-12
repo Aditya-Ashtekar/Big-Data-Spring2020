b591141c2f32dfc2fe67d88ef9ba7e1c62e77caf
Aditya Ashtekar <adityaashtekar2012@gmail.com>
2020-03-11 20:21:55 -0400

    updated task2b

--- assignment2/task2c.py
+++ assignment2/task2c.py
@@ -7,8 +7,9 @@ from csv import reader
 AllTrips = sc.textFile("/user/ama1219/task1a.out")
 AllTrips = AllTrips.mapPartitions(lambda x:reader(x))
 
-result= AllTrips.map(lambda x: (x[3],(str(round(float(x[15])+float(x[16])+float(x[18]),2)),str(round(float(x[19]),2)))))
+result= AllTrips.map(lambda x: (x[3].split()[0],(float(x[15])+float(x[16])+float(x[18]),float(x[19]))))
+result = result.reduceByKey(lambda x,y : (x[0]+y[0],x[1]+y[1]))
 result = result.sortByKey()
-result = result.map(lambda x: (x[0],x[1][0],x[1][1]))
+result = result.map(lambda x: (x[0],str(round(x[1][0],2)),str(round(x[1][1],2))))
 output = result.map(lambda r: ', '.join([KVPair for KVPair in r]))
 output.saveAsTextFile('task2c.out')
