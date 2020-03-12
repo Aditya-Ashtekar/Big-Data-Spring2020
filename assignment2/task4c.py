a7b365f5600334fc5a6f782711ab274c3a766962
Aditya-Ashtekar <ama1219@nyu.edu>
2020-03-11 20:28:44 -0400

    Updated argument

--- assignment2/task4c.py
+++ assignment2/task4c.py
@@ -3,8 +3,7 @@ import pyspark
 from pyspark import SparkContext
 sc = SparkContext.getOrCreate("")
 from csv import reader
-Licenses = sc.textFile("task1b.out",1)
-#Licenses = sc.textFile(sys.argv[1],1)
+Licenses = sc.textFile(sys.argv[1],1)
 Licenses = Licenses.mapPartitions(lambda x:reader(x))
 
 Agent = Licenses.map(lambda x: (x[20],x[5])).reduceByKey(lambda x,y: str(round(float(x)+float(y),2))).sortBy(lambda x: (x[1],x[0]),ascending=False)
