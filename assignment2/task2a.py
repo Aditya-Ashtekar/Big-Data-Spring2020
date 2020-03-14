e266a4ee378280fcee85240e5555efacf2ad67e4
Aditya Ashtekar <adityaashtekar2012@gmail.com>
2020-03-14 00:49:10 -0400

    updated task2a to count correctly for >100

--- assignment2/task2a.py
+++ assignment2/task2a.py
@@ -6,9 +6,10 @@ from csv import reader
 AllTrips = sc.textFile(sys.argv[1],1)
 AllTrips = AllTrips.mapPartitions(lambda x:reader(x))
 
-mapped = AllTrips.map(lambda x: ((0,5),1) if (float(x[15])>0 and float(x[15])<=5) else (((5,15),1) if float(x[15])>5 and float(x[15])<=15 else(((15,30),1) if float(x[15])>15 and float(x[15])<=30 else (((30,50),1)if float(x[15])>30 and float(x[15])<=50 else (((50,100),1)if float(x[15])>50 and float(x[15])<=100 else ((100,sys.maxsize),1))))))
+mapped = AllTrips.map(lambda x: ((0,5),1) if (float(x[15])>0 and float(x[15])<=5) else (((5,15),1) if float(x[15])>5 and float(x[15])<=15 else(((15,30),1) if float(x[15])>15 and float(x[15])<=30 else (((30,50),1)if float(x[15])>30 and float(x[15])<=50 else (((50,100),1)if float(x[15])>50 and float(x[15])<=100 else (((100,sys.maxsize),1) if float(x[15])>100 else((-1,-1),1) ))))))
 res = mapped.countByKey()
 res = sc.parallelize(list(res.items()))
+res = res.filter(lambda x: x[0][0] != -1)
 result = res.map(lambda x : (x[0][0],x[0][1],x[1]))
 
 output = result.map(lambda r: ', '.join([str(KVPair) for KVPair in r]))
